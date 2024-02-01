dea07819ec70f3e5f1013e294d168e71caf887a0
Dinastia-Harzu <dinastiadelah@gmail.com>
2024-02-01 11:46:36 +0100

    Establecer único punto de acceso

--- index.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-// Llamar al controlador ControladorPrincipal
---
+++ publico/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arturo García Richardson</title>
+</head>
+<body>
+    <h1>Único punto de acceso</h1>
+</body>
+</html>
