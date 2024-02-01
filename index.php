89dc7a7aa4fb102b9bc71a5f6fae5f5d95af293c
Dinastia-Harzu <dinastiadelah@gmail.com>
2024-02-01 11:29:33 +0100

    Planificar MVC

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Portfolio</title>
-</head>
-<body>
-    <h1>Hola desde index</h1>
-</body>
-</html>
+<?php
+
+// Llamar al controlador ControladorPrincipal
